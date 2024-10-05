085837c745757dc625fb1d9e257a662517005ac1
Alif <141668626+22f3655@users.noreply.github.com>
2024-10-05 16:10:10 +0500

    ...

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-	public static void main(String[] args) {
-		Library myLibrary = new Library();
-		myLibrary.addBook("Harry Potter);
-		myLibrary.addBook("Lord of the Rings");
-		myLibrary.listBooks();
-	}
-}
