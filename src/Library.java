43d9645e782caeeb55c9e529da1bd1b5ce83457a
Alif <141668626+22f3655@users.noreply.github.com>
2024-10-05 16:19:45 +0500

    Added searchBook function to Library class

--- src/Library.java
+++ src/Library.java
@@ -12,4 +12,8 @@ public class Library {
 	public void listBooks() {
 		System.out.println("Books in library: " + books);
 	}
+
+	public boolean searchBook(String bookName) {
+		return books.contains(bookName);
+	}
 }
